f428573b6bbf030541e8d885fbf361e86fe9bc0f
Sander Eek <46729996+sandereek@users.noreply.github.com>
2020-04-14 10:31:16 +0200

    Add files via upload

---
+++ include files/FTP_Functions.ps1
@@ -0,0 +1,84 @@
+# Upload Single File
+$FTPServer = "ftp://example.com/"
+$FTPUsername = "username"
+$FTPPassword = "password" 
+$LocalDirectory = "C:\temp\" 
+$FileToUpload = "example.txt"
+
+#Connect to the FTP Server
+$ftp = [System.Net.FtpWebRequest]::create("$FTPServer/$FileToUpload")
+$ftp.Credentials =  New-Object System.Net.NetworkCredential($FTPUsername,$FTPPassword)
+
+#Upload file to FTP Server
+$ftp.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+
+#Verify the file was uploaded
+$ftp.GetResponse()
+
+# Delete Single File
+$FTPServer = "ftp://example.com/"
+$FTPUsername = "username"
+$FTPPassword = "password" 
+$LocalDirectory = "C:\temp\" 
+$FileToDelete = "example.txt"
+
+#Connect to the FTP Server
+$ftp = [System.Net.FtpWebRequest]::create("$FTPServer/$FileToDelete")
+$ftp.Credentials =  New-Object System.Net.NetworkCredential($FTPUsername,$FTPPassword)
+
+#Delete file on FTP Server
+$ftp.Method = [System.Net.WebRequestMethods+Ftp]::DeleteFile
+
+#Verify the file was uploaded
+$ftp.GetResponse()
+
+# Upload every file in a directory
+#Unique Variables
+$FTPServer = "ftp://example.com/"
+$FTPUsername = "username"
+$FTPPassword = "password" 
+$LocalDirectory = "C:\temp\" 
+
+#Loop through every file
+foreach($FileToUpload in (dir $LocalDirectory "*")){ 
+
+    #Connect to the FTP Server
+    $ftp = [System.Net.FtpWebRequest]::create("$FTPServer/$FileToUpload")
+    $ftp.Credentials =  New-Object System.Net.NetworkCredential($FTPUsername,$FTPPassword)
+
+    #Upload file to FTP Server
+    $ftp.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+
+    #Verify the file was uploaded
+    $ftp.GetResponse()
+}
+
+# View files on FTP Server
+
+#Unique Variables
+$FTPServer = "ftp://homaasftp.westus.cloudapp.azure.com"
+$FTPUsername = "test"
+$FTPPassword = "test" 
+
+#Connection String
+$credentials = new-object System.Net.NetworkCredential($FTPUsername, $FTPPassword)
+
+#function to view the files in the FTP Server
+function View-Files-in-FTP-Server ($url,$credentials) {
+    $request = [Net.WebRequest]::Create($url)
+    $request.Method = [System.Net.WebRequestMethods+FTP]::ListDirectory
+    if ($credentials) { $request.Credentials = $credentials }
+    $response = $request.GetResponse()
+    $reader = New-Object IO.StreamReader $response.GetResponseStream() 
+    $reader.ReadToEnd()
+    $reader.Close()
+    $response.Close()
+}
+
+#Use the root directory of the FTP Server
+$ListFiles = View-Files-in-FTP-Server -url $FTPServer -credentials $credentials
+$files = ($ListFiles -split "`r`n")
+
+Write-Host 'The Files on the FTP Server : '
+Write-Host `r
+$files 
