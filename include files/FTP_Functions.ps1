d274f51b7d65d1d2a4cacc0013f4d69878858124
Sander Eek <46729996+sandereek@users.noreply.github.com>
2020-04-14 17:38:12 +0200

    Generic Include Files

--- include files/FTP_Functions.ps1
+++ include files/FTP_Functions.ps1
@@ -24,11 +24,11 @@
 Function FTPDownload() {
     param
     (
-       [Parameter(Mandatory = $true)] [string] $FTPServer,
-       [Parameter(Mandatory = $true)] [string] $FTPUsername,
-       [Parameter(Mandatory = $true)] [string] $FTPPassword,
-       [Parameter(Mandatory = $true)] [string] $FTPDownloadDirectory,
-       [Parameter(Mandatory = $true)] [string] $FTPFileToDownload
+        [Parameter(Mandatory = $true)] [string] $FTPServer,
+        [Parameter(Mandatory = $true)] [string] $FTPUsername,
+        [Parameter(Mandatory = $true)] [string] $FTPPassword,
+        [Parameter(Mandatory = $true)] [string] $FTPDownloadDirectory,
+        [Parameter(Mandatory = $true)] [string] $FTPFileToDownload
     )
 
     write-host "$(get-date) FTPServer: " $FTPServer
@@ -37,20 +37,34 @@ Function FTPDownload() {
     write-host "$(get-date) FTPDownloadDirectory: " $FTPDownloadDirectory
     write-host "$(get-date) FTPFileToDownload: "$FTPFileToDownload
 
-#Connect to the FTP Server
-$ftp = [System.Net.FtpWebRequest]::create("$FTPServer/$FTPFileToDownload")
-$ftp.Credentials =  New-Object System.Net.NetworkCredential($FTPUsername,$FTPPassword)
-
-#Upload file to FTP Server
-$ftp.Method = [System.Net.WebRequestMethods+Ftp]::DownloadFile
-$FTPRequest.UseBinary = $true
-$FTPRequest.KeepAlive = $false
-
-#Verify the file was uploaded
-$ftp.GetResponse()
+    # Config
+    $Username = "FTPUSER"
+    $Password = "P@assw0rd"
+    $LocalFile = "C:\Temp\file.zip"
+    $RemoteFile = "ftp://thomasmaurer.ch/downloads/files/file.zip"
+ 
+    # Create a FTPWebRequest
+    $FTPRequest = [System.Net.FtpWebRequest]::Create($RemoteFile)
+    $FTPRequest.Credentials = New-Object System.Net.NetworkCredential($Username, $Password)
+    $FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::DownloadFile
+    $FTPRequest.UseBinary = $true
+    $FTPRequest.KeepAlive = $false
+    # Send the ftp request
+    $FTPResponse = $FTPRequest.GetResponse()
+    # Get a download stream from the server response
+    $ResponseStream = $FTPResponse.GetResponseStream()
+    # Create the target file on the local system and the download buffer
+    $LocalFileFile = New-Object IO.FileStream ($LocalFile, [IO.FileMode]::Create)
+    [byte[]]$ReadBuffer = New-Object byte[] 1024
+    # Loop through the download
+    do {
+        $ReadLength = $ResponseStream.Read($ReadBuffer, 0, 1024)
+        $LocalFileFile.Write($ReadBuffer, 0, $ReadLength)
+    }
+    while ($ReadLength -ne 0)
 }
 
-$FTPServer = "ftp://homaasfromhome.westus2.cloudapp.azure.com/"
+$FTPServer = "ftp://homaasfromhome.westus2.cloudapp.azure.com"
 $FTPUsername = "homaasftp"
 $FTPPassword = "123Hom@@s!" 
 $FTPDownloadDirectory = "C:\temp\" 
