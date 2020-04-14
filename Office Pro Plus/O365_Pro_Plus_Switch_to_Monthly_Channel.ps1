121a4932c7d1b4ec8e138d62bf2032cb45b625ad
Sander Eek <seek@anwb.nl>
2020-04-14 15:17:18 +0200

    Office Pro Plus

--- Office Pro Plus/O365_Pro_Plus_Switch_to_Monthly_Channel.ps1
+++ Office Pro Plus/O365_Pro_Plus_Switch_to_Monthly_Channel.ps1
@@ -1,25 +1,32 @@
+<#
+.SYNOPSIS
+    Configures the Registry for Monthly Office 365 Pro Plus Updates
+.DESCRIPTION
+    This Scripts configures the registry to configure Office 365 Pro Plus to use Monthly updates
+.PARAMETERS 
+    None
+.INPUTS
+    None
+.OUTPUTS
+    None
+.NOTES
+  Version:        1.0
+  Author:         Sander Eek
+  Creation Date:  14 april 2020
+  Purpose/Change: Initial script development (converted from CMD file)
+  Last Update:    14 april 2020
+  
+.EXAMPLE
+  .\O365_Pro_Plus_Switch_to_Monthly_Channel.ps1
+#>
 
+$ErrorActionPreference = "SilentlyContinue"
 
-
-# Check if key CDNBaseUrl
-# exists under HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration\
-
-# If Yes, then the computer is already configured for Monthly updates
-
-# If No,
-# Under HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration
+# Check if key CDNBaseUrl. Under HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration
 # add CDNBaseUrl as REG_SZ with value: http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60
-
-$registryPath = 
-$registryName = 
-$registryValue =
-$registryPropertyType = 
-
-
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration" -Name "CDNBaseUrl" `
 -Value "http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60"  -PropertyType "String"  -Force
 
-# Delete the following values:
 # Under HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration
 # Delete UpdateUrl and UpdateToVersion
 Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration" -Name "UpdateUrl" -Confirm
@@ -29,5 +36,8 @@ Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configurat
 # Delete UpdateToVersion
 Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Updates" -Name "UpdateToVersion" -Confirm
 
-# Delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Office\16.0\Common\OfficeUpdate
+# Under HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Office\16.0\Common
+# Delete OfficeUpdate
 Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Office\16.0\Common" -Name "OfficeUpdate" -Confirm
+
+$ErrorActionPreference = "Continue"
--- Office Pro Plus/readme.md
+++ Office Pro Plus/readme.md
@@ -1,1 +1,1 @@
-
+This folder contains PowerShell Scripts for managing Office 365 Pro Plus
